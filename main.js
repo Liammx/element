c8cbbf638f1f3ab81c9582b6ae0837a59023d7cf
liammx <liammx03@gmial.com>
2023-11-30 14:46:52 +0500

    element

---
+++ main.js
@@ -0,0 +1,82 @@
+const item1 = document.querySelector('.item1')
+const item2 = document.querySelector('.item2')
+const item3 = document.querySelector('.item3')
+const item4 = document.querySelector('.item4')
+const item5 = document.querySelector('.item5')
+const item6 = document.querySelector('.item6')
+const item7 = document.querySelector('.item7')
+const item8 = document.querySelector('.item8')
+const item9 = document.querySelector('.item9')
+const item10 = document.querySelector('.item10')
+const box1 = document.querySelector('.box1')
+
+item1.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 1'
+    box.classList.add('p2')
+})
+item2.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 2'
+    box.classList.add('p2')
+})
+item3.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 3'
+    box.classList.add('p2')
+})
+item4.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 4'
+    box.classList.add('p2')
+})
+item5.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 5'
+    box.classList.add('p2')
+})
+item6.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 6'
+    box.classList.add('p2')
+})
+item7.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 7'
+    box.classList.add('p2')
+})
+item8.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 8'
+    box.classList.add('p2')
+})
+item9.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 9'
+    box.classList.add('p2')
+})
+item10.addEventListener('click', () => {
+    let box = document.createElement('div')
+    box.classList.add('klon')
+    box1.appendChild(box)
+    box.innerText = 'copy element 10'
+    box.classList.add('p2')
+})
